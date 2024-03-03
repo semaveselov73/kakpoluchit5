94f9984a8bb1127be5418903f0fa20315023ffdd
semaveselov <sema.veselov.2017@gmail.com>
2024-03-03 18:29:43 +0300

    main

---
+++ main/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program {
+    public static void Main(string[] args) {
+        getSum();
+    }
+
+    private static void getSum() {
+        Console.WriteLine("Введите числа.");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        int sum = num1 + num2;
+        Console.WriteLine("Сумма равна " + sum);
+    }
+}
