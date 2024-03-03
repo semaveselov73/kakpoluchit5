42ba2da8f3e7cd69eb185a2185b3132f29d147e9
semaveselov <sema.veselov.2017@gmail.com>
2024-03-03 18:44:17 +0300

    new ver

--- main/Program.cs
+++ main/Program.cs
@@ -4,8 +4,9 @@
     }
 
     private static void getSum() {
-        Console.WriteLine("Введите числа.");
+        Console.WriteLine("Введите 1 число.");
         int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите 2 число.");
         int num2 = Convert.ToInt32(Console.ReadLine());
         int sum = num1 + num2;
         Console.WriteLine("Сумма равна " + sum);
